161ddc1ed5106b33ae8a9496502b2179e2897704
fendywongso <Fendy.Wongso@datacom.com.au>
2019-02-07 21:37:12 +1100

    fix bugs

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ restService.post("/echo", function(req, res) {
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
       ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again.";
+      : "It seems like some problem occurred. Please Speak again.";
   return res.json({
       "fulfillment_text": speech,
       "payload": {
